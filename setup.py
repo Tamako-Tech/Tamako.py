4004d770a4c4d477a5d550592edb5c39d0a2cc5f
S4qib <73705449+S4qib@users.noreply.github.com>
2021-02-04 11:19:33 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
-from distutils.core import setup
-setup(
-  name = 'Tamako.py',         # How you named your package folder (MyLib)
-  packages = ['Tamako.py'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A simple package to interact with the Tamako API!',   # Give a short description about your library
-  author = 'daftscientist',                   # Type in your name
-  author_email = 'daftscientist@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/DaftDevelopment/Tamako.py',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/DaftDevelopment/Tamako.py/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['tamako', 'Tamako', 'ChatBot'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'requests'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.9.1'
-  ],
+import setuptools
+
+with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Better-Tamako.py",
+    version="0.2",
+    author="S4qib",
+    description="Simple API Wrapper for Tamako API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/S4qib/Better-Tamako.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>= 3.6',
+    include_package_data=True,
+    install_requires=["requests"]
 )
