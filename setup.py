ab4a80958b0b9eec516977bf52f98dc7ffaf0da5
BearTS <bear@tamako.tech>
2021-02-08 01:38:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Tamako.py',         # How you named your package folder (MyLib)
   packages = ['Tamako.py'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple package to interact with the Tamako API!',   # Give a short description about your library
   author = 'daftscientist',                   # Type in your name
   author_email = 'daftscientist@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/DaftDevelopment/Tamako.py',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/DaftDevelopment/Tamako.py/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/Tamako-Tech/Tamako.py',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Tamako-Tech/Tamako.py/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['tamako', 'Tamako', 'ChatBot'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
